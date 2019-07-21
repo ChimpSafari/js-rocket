200a119cd1bb42cf45e523bfb3474968afc96075
Aaron Yamoah <aaron.yamoah@live.com>
2019-07-21 16:33:14 +0100

    split into CSS and JS files

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var sayMyName = function(name){
+    alert ('My name is '+name);
+}
+
+
+var car = {
+    make: 'VW',
+    type: 'Polo',
+    color: 'blue',
+    isTurnedOn: false,
+    numberOfWheels: 4,
+    seats:[
+        'seat1',
+        'seat2',
+        'seat3',
+        'seat4'
+    ],
+    turnOn: function () {
+        this.isTurnedOn = true;
+        console.log(this.isTurnedOn)
+    },
+    fly: function () {
+        alert('fly');
+    },
+    switchCar: function(isOn) {
+        console.log('Turn car ' +isOn)
+        if (isOn == true) {
+            this.isTurnedOn = true;
+        } else {
+            this.isTurnedOn = false;
+        } 
+
+        console.log(this.isTurnedOn);
+    }
+};
+    console.log("hello there friends!!!");
